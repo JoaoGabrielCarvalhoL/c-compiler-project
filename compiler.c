b075001c32b9f2ecd458a04a57aceffe51c12116
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2022-12-06 13:33:48 -0300

    Implementation number token

--- compiler.c
+++ compiler.c
@@ -1,4 +1,6 @@
 #include "compiler.h"
+#include <stdarg.h>
+#include <stdlib.h>
 
 struct lex_process_functions compiler_lex_functions = {
     .next_char = compile_process_next_char, 
@@ -31,4 +33,26 @@ int compile_file(const char* filename, const char* out_filename, int flags)
     //Preform code generation
 
     return COMPILER_FILE_COMPILED_OK;
+}
+
+void compiler_error(struct compile_process* compiler, const char* msg, ...) 
+{
+    va_list args; 
+    va_start(args, msg);
+    vfprintf(stderr, msg, args); 
+    va_end(args);
+
+    fprintf(stderr, " on line %i, col %i in file %s\n", compiler->pos.line, compiler->pos.col, compiler->pos.filename);
+    exit(-1);
+}
+
+void compiler_warning(struct compile_process* compiler, const char* msg, ...) 
+{
+    va_list args; 
+    va_start(args, msg);
+    vfprintf(stderr, msg, args); 
+    va_end(args);
+
+    fprintf(stderr, " on line %i, col %i in file %s\n", compiler->pos.line, compiler->pos.col, compiler->pos.filename);
+   
 }
